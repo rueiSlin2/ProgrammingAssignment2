2cfa5a68865904b276d95bb38fd63366ae0deb33
rueiSlin2 <lin.ruei.peter@gmail.com>
2015-01-24 15:07:35 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix() function can be used to generate a new vector
+## containing information of an invertible matrix and an indicator stating
+## if the matix has been calculated.
+## Example 1a (new setting mode): 
+## > newVec <- makeCacheMatrix(matrix(1:4,2,2))
+## Example 1b (resetting mode):
+## > usedVec$set(matrix(1:4,2,2))
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  IMTX <- NULL
+  set <- function(y) {
+    x <<- y
+    IMTX <<- NULL
+  }
+  
+  get <- function() x
+  setIMTX <- function(invM) IMTX <<- invM
+  getIMTX <- function() IMTX
+  list(set = set, get = get,
+       setIMTX = setIMTX,
+       getIMTX = getIMTX)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve() function can take the vector made by makeCacheMatrix() as
+## an argument to calculate the inverse matrix and store the result in the vector.
+## Example:
+## > cacheSolve(newVec)
+##     [,1] [,2]
+##[1,]   -2  1.5
+##[2,]    1 -0.5
+
+## If the same vector is processed by cacheSolve() again, the stored result will
+## show up rather than repeat the calculation.
+## Example:
+## > cacheSolve(newVec) 'the 2nd time'
+## getting cached data
+##      [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  IMTX <- x$getIMTX()
+  if(!is.null(IMTX)) {
+    message("getting cached data")
+    return(IMTX)
+  }
+  data <- x$get()
+  IMTX <- solve(data, ...)
+  x$setIMTX(IMTX)
+  IMTX
+## Return a matrix that is the inverse of 'x'
 }
